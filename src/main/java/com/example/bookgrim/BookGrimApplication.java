9db6b6186d899e13b8129598d2bcf0ecc5f77759
WON <wondang99@naver.com>
2023-04-05 12:44:15 +0900

    Initial Commit

---
+++ src/main/java/com/example/bookgrim/BookGrimApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookgrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookGrimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookGrimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookgrim/BookGrimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookgrim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookGrimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
