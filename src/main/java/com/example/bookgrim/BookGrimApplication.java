1a1766e2b8a46c19809ec334bc4a8514555b9710
WON <wondang99@naver.com>
2023-05-05 21:45:29 +0900

    [feat] : Add automatically created, updated time of  Entity

--- src/main/java/com/example/bookgrim/BookGrimApplication.java
+++ src/main/java/com/example/bookgrim/BookGrimApplication.java
@@ -2,8 +2,10 @@ package com.example.bookgrim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookGrimApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookGrimApplication.class, args);
